9ac7268e8f3181f14462d0ed294c8bd737a844ba
flyguide <flyguide@735d13b6-9817-0410-8766-e36946ffe9aa>
2005-10-19 19:20:21 +0000

    rendering now works like regular content elements
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/irfaq/trunk@777 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = Array (
 	'TYPO3_version' => '3.6.2-3.8.2',
 	'PHP_version' => '4.1.2-5.0.5',
 	'module' => '',
-	'state' => 'beta',
+	'state' => 'stable',
 	'internal' => 0,
 	'uploadfolder' => 0,
 	'createDirs' => '',
@@ -35,7 +35,7 @@ $EM_CONF[$_EXTKEY] = Array (
 	'CGLcompliance_note' => '',
 	'private' => 0,
 	'download_password' => '',
-	'version' => '0.4.8',	// Don't modify this! Managed automatically during upload to repository.
+	'version' => '0.4.9',	// Don't modify this! Managed automatically during upload to repository.
 	'_md5_values_when_last_written' => 'a:30:{s:9:"ChangeLog";s:4:"c24e";s:12:"ext_icon.gif";s:4:"fc45";s:17:"ext_localconf.php";s:4:"6c92";s:15:"ext_php_api.dat";s:4:"78a3";s:14:"ext_tables.php";s:4:"470f";s:14:"ext_tables.sql";s:4:"f6d5";s:28:"ext_typoscript_constants.txt";s:4:"0791";s:24:"ext_typoscript_setup.txt";s:4:"cca0";s:15:"flexform_ds.xml";s:4:"add8";s:21:"icon_tx_irfaq_cat.gif";s:4:"cce6";s:24:"icon_tx_irfaq_expert.gif";s:4:"cdfb";s:19:"icon_tx_irfaq_q.gif";s:4:"fc45";s:27:"icon_tx_irfaq_sysfolder.gif";s:4:"8f2c";s:13:"locallang.php";s:4:"8a86";s:16:"locallang_db.php";s:4:"b804";s:7:"tca.php";s:4:"6123";s:14:"doc/manual.sxw";s:4:"dc0d";s:19:"doc/wizard_form.dat";s:4:"f97e";s:20:"doc/wizard_form.html";s:4:"4cbc";s:14:"pi1/ce_wiz.gif";s:4:"02b6";s:26:"pi1/class.tx_irfaq_pi1.php";s:4:"88cf";s:34:"pi1/class.tx_irfaq_pi1_wizicon.php";s:4:"bc95";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.php";s:4:"623c";s:24:"pi1/static/editorcfg.txt";s:4:"a434";s:20:"pi1/static/setup.txt";s:4:"c8fa";s:14:"res/irfaq.tmpl";s:4:"0170";s:13:"res/minus.gif";s:4:"769b";s:12:"res/plus.gif";s:4:"29d0";s:16:"res/toggleFaq.js";s:4:"0d15";}',
 );
 
