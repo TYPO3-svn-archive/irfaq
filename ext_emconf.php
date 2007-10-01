886647083daf7c5fbb6fb49877e30d0a278297cc
patrick <patrick@735d13b6-9817-0410-8766-e36946ffe9aa>
2007-10-01 13:04:01 +0000

    Change in version number
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/irfaq/trunk@6773 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "irfaq"
 #
-# Auto generated 28-09-2007 10:28
+# Auto generated 01-10-2007 13:53
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'FAQ frontend plugin with dynamic or static view which will merge and improve functionality of EXT:faq and EXT:faq_plus into a modern look',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '1.0.0',
+	'version' => '1.0.1',
 	'dependencies' => 'cms',
 	'conflicts' => '',
 	'priority' => '',
@@ -43,7 +43,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:50:{s:9:"ChangeLog";s:4:"52b9";s:26:"class.tx_irfaq_tcemain.php";s:4:"9f71";s:12:"ext_icon.gif";s:4:"fc45";s:17:"ext_localconf.php";s:4:"3764";s:15:"ext_php_api.dat";s:4:"78a3";s:14:"ext_tables.php";s:4:"d881";s:14:"ext_tables.sql";s:4:"fa71";s:28:"ext_typoscript_constants.txt";s:4:"0791";s:24:"ext_typoscript_setup.txt";s:4:"e58b";s:15:"flexform_ds.xml";s:4:"25ee";s:21:"icon_tx_irfaq_cat.gif";s:4:"cce6";s:24:"icon_tx_irfaq_expert.gif";s:4:"cdfb";s:19:"icon_tx_irfaq_q.gif";s:4:"fc45";s:27:"icon_tx_irfaq_sysfolder.gif";s:4:"8f2c";s:13:"locallang.php";s:4:"5a5d";s:16:"locallang_db.php";s:4:"2e15";s:7:"tca.php";s:4:"f3bd";s:14:"doc/manual.sxw";s:4:"dae4";s:19:"doc/wizard_form.dat";s:4:"f97e";s:20:"doc/wizard_form.html";s:4:"4cbc";s:21:"lang/de.locallang.xml";s:4:"7579";s:24:"lang/de.locallang_db.xml";s:4:"95ea";s:21:"lang/dk.locallang.xml";s:4:"96ab";s:24:"lang/dk.locallang_db.xml";s:4:"9faf";s:21:"lang/fi.locallang.xml";s:4:"16a9";s:24:"lang/fi.locallang_db.xml";s:4:"c77a";s:21:"lang/it.locallang.xml";s:4:"7591";s:24:"lang/it.locallang_db.xml";s:4:"f369";s:18:"lang/locallang.xml";s:4:"3252";s:21:"lang/locallang_db.xml";s:4:"8291";s:21:"lang/nl.locallang.xml";s:4:"6833";s:24:"lang/nl.locallang_db.xml";s:4:"f854";s:14:"pi1/ce_wiz.gif";s:4:"02b6";s:26:"pi1/class.tx_irfaq_pi1.php";s:4:"13b4";s:34:"pi1/class.tx_irfaq_pi1_wizicon.php";s:4:"aacd";s:13:"pi1/clear.gif";s:4:"cc11";s:20:"pi1/de.locallang.xml";s:4:"5f75";s:20:"pi1/dk.locallang.xml";s:4:"3e1a";s:20:"pi1/fi.locallang.xml";s:4:"9169";s:20:"pi1/it.locallang.xml";s:4:"5f8d";s:17:"pi1/locallang.php";s:4:"965c";s:17:"pi1/locallang.xml";s:4:"1368";s:20:"pi1/nl.locallang.xml";s:4:"ca4d";s:24:"pi1/static/editorcfg.txt";s:4:"a434";s:20:"pi1/static/setup.txt";s:4:"c8fa";s:14:"res/irfaq.tmpl";s:4:"2dab";s:13:"res/minus.gif";s:4:"769b";s:12:"res/plus.gif";s:4:"29d0";s:16:"res/toggleFaq.js";s:4:"0d15";s:20:"static/css/setup.txt";s:4:"85c1";}',
+	'_md5_values_when_last_written' => 'a:50:{s:9:"ChangeLog";s:4:"f9d5";s:26:"class.tx_irfaq_tcemain.php";s:4:"9f71";s:12:"ext_icon.gif";s:4:"fc45";s:17:"ext_localconf.php";s:4:"3764";s:15:"ext_php_api.dat";s:4:"78a3";s:14:"ext_tables.php";s:4:"d881";s:14:"ext_tables.sql";s:4:"fa71";s:28:"ext_typoscript_constants.txt";s:4:"0791";s:24:"ext_typoscript_setup.txt";s:4:"e58b";s:15:"flexform_ds.xml";s:4:"25ee";s:21:"icon_tx_irfaq_cat.gif";s:4:"cce6";s:24:"icon_tx_irfaq_expert.gif";s:4:"cdfb";s:19:"icon_tx_irfaq_q.gif";s:4:"fc45";s:27:"icon_tx_irfaq_sysfolder.gif";s:4:"8f2c";s:13:"locallang.php";s:4:"5a5d";s:16:"locallang_db.php";s:4:"2e15";s:7:"tca.php";s:4:"f3bd";s:14:"doc/manual.sxw";s:4:"dae4";s:19:"doc/wizard_form.dat";s:4:"f97e";s:20:"doc/wizard_form.html";s:4:"4cbc";s:21:"lang/de.locallang.xml";s:4:"7579";s:24:"lang/de.locallang_db.xml";s:4:"95ea";s:21:"lang/dk.locallang.xml";s:4:"96ab";s:24:"lang/dk.locallang_db.xml";s:4:"9faf";s:21:"lang/fi.locallang.xml";s:4:"16a9";s:24:"lang/fi.locallang_db.xml";s:4:"c77a";s:21:"lang/it.locallang.xml";s:4:"7591";s:24:"lang/it.locallang_db.xml";s:4:"f369";s:18:"lang/locallang.xml";s:4:"3252";s:21:"lang/locallang_db.xml";s:4:"8291";s:21:"lang/nl.locallang.xml";s:4:"6833";s:24:"lang/nl.locallang_db.xml";s:4:"f854";s:14:"pi1/ce_wiz.gif";s:4:"02b6";s:26:"pi1/class.tx_irfaq_pi1.php";s:4:"1395";s:34:"pi1/class.tx_irfaq_pi1_wizicon.php";s:4:"aacd";s:13:"pi1/clear.gif";s:4:"cc11";s:20:"pi1/de.locallang.xml";s:4:"5f75";s:20:"pi1/dk.locallang.xml";s:4:"3e1a";s:20:"pi1/fi.locallang.xml";s:4:"9169";s:20:"pi1/it.locallang.xml";s:4:"5f8d";s:17:"pi1/locallang.php";s:4:"965c";s:17:"pi1/locallang.xml";s:4:"1368";s:20:"pi1/nl.locallang.xml";s:4:"ca4d";s:24:"pi1/static/editorcfg.txt";s:4:"a434";s:20:"pi1/static/setup.txt";s:4:"c8fa";s:14:"res/irfaq.tmpl";s:4:"22fb";s:13:"res/minus.gif";s:4:"769b";s:12:"res/plus.gif";s:4:"29d0";s:16:"res/toggleFaq.js";s:4:"0d15";s:20:"static/css/setup.txt";s:4:"85c1";}',
 	'suggests' => array(
 	),
 );
